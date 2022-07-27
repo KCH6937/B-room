a14cfbd0b543a04abaf2f554effc07961d329560
KCH6937 <adasddasd12@naver.com>
2022-07-27 19:25:05 +0900

    [Feat] #6 - 회원가입, 로그인, 인증 부분 메시지 추가

--- src/modules/message.ts
+++ src/modules/message.ts
@@ -1,11 +1,21 @@
 const message = {
-  SUCCESS: "성공",
-  NULL_VALUE: "필요한 값이 없음",
-  NOT_FOUND: "존재하지 않는 자원",
-  BAD_REQUEST: "잘못된 요청",
-  INTERNAL_SERVER_ERROR: "서버 내부 오류",
+  SUCCESS: '성공',
+  NULL_VALUE: '필요한 값이 없음',
+  NOT_FOUND: '존재하지 않는 자원',
+  BAD_REQUEST: '잘못된 요청',
+  INTERNAL_SERVER_ERROR: '서버 내부 오류',
 
-  DUPLICATE_EMAIL: "이미 존재하는 이메일",
+  // (사용자) 회원가입 부분
+  DUPLICATE_EMAIL: '이미 존재하는 이메일',
+
+  // (사용자) 로그인 부분
+  INVALID_USER_INFO: '존재하지 않는 회원정보',
+  INVALID_DEPARTMENT_INFO: '존재하지 않는 부서명',
+
+  // (Access Token) 인증 부분
+  NULL_VALUE_ACEESS_TOKEN: '토큰 필요',
+  ACCESS_TOKEN_EXPIRES: '토큰 만료',
+  INVALID_ACCESS_TOKEN: '토큰 검증 실패'
 };
 
 export default message;
