401e787a1921d47e72fa880dd85180d6990792c4
KCH6937 <adasddasd12@naver.com>
2022-07-27 19:49:49 +0900

    [Feat] #6 - Line 9: /users 라우터 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import TestRouter from '@routes/test.route';
 import departmentRouter from '@routes/department/department.route';
+import userRouter from '@routes/user/user.route';
 
 const router = Router();
 router.use('/test', TestRouter);
 router.use('/department', departmentRouter);
+router.use('/users', userRouter);
 
 export default router;
