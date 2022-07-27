c33ab4bfc4bbc9c90c3c14bf72ca290dd73076e9
KCH6937 <adasddasd12@naver.com>
2022-07-27 19:48:39 +0900

    [Style] #6 - 디렉터리 구조 변경

--- src/routes/user.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import userController from '@controllers/user.controller';
-
-const router: Router = Router();
-
-router.post('/register', userController.register);
-router.post('/login', userController.login);
-
-export default router;
