9176e8efc0d93c74d63fe918e654bbe0193c8aae
KCH6937 <adasddasd12@naver.com>
2022-07-27 19:36:55 +0900

    [Refactor] #6 - setError 리팩토링

--- src/modules/setError.ts
+++ src/modules/setError.ts
@@ -1,12 +1,10 @@
 import { ResError } from '@interfaces/common/resError.dto';
 
 const setError = (statusCode: number, message: string) => {
-  const resErr: ResError = {
-    statusCode,
-    message
-  };
+  const error: ResError = new Error(message);
+  error.statusCode = statusCode;
 
-  return resErr;
+  return error;
 };
 
 export default setError;
