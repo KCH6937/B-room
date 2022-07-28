34f5c21e92865a5ea271e299a8357de4e83863af
whoamixzerone <whoamixzerone@gmail.com>
2022-07-28 15:29:27 +0900

    [Feat]#14 - 공지사항 서비스 구현

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import departmentRouter from '@routes/department/department.route';
 import userRouter from '@routes/user/user.route';
 import companyNoticeRouter from '@routes/companyNotice/companyNotice.route';
+import auth from '@middlewares/auth.middlewares';
 
-const router = Router();
+const router: Router = Router();
+
 router.use('/department', departmentRouter);
 router.use('/users', userRouter);
-router.use('/notices', companyNoticeRouter);
+router.use('/notices', auth.authJWT, companyNoticeRouter);
 
 export default router;
