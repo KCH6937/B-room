b3fae9cef42783055475c0c9f9087b636a22f1db
Woody-Park <starface1009@gmail.com>
2022-07-29 11:42:00 +0900

    [Rename] #26 - 파일명 중복 수정 업데이트

--- src/controllers/companyChatRoom/companyChatRoom.controller.ts
+++ src/controllers/companyChatRoom/companyChatRoom.controller.ts
@@ -1,13 +1,15 @@
 import { Request, Response } from 'express';
 import statusCode from '@modules/statusCode';
-import { success, fail } from '@modules/response';
-import message from '@modules/message';
+import { fail } from '@modules/response';
+import {
+  CreateChatRoomInputDto,
+  CreateChatRoomDto
+} from '@interfaces/companyChat/createChatRoom.dto';
+import companyChatRoomService from '@services/companyChatRoom/companyChatRoom.service';
 
-import companyChatroomService from '@services/companyChatRoom/companyChatRoom.service';
-
-const getChatroomsInfo = async (req: Request, res: Response) => {
+const getChatRoomsInfo = async (req: Request, res: Response) => {
   try {
-    const result = await companyChatroomService.getChatroomsInfo();
+    const result = await companyChatRoomService.getChatRoomsInfo();
     return res.status(statusCode.OK).json(result);
   } catch (error: any) {
     return res
@@ -16,6 +18,31 @@ const getChatroomsInfo = async (req: Request, res: Response) => {
   }
 };
 
+const createChatRoom = async (req: Request, res: Response) => {
+  const createChatRoomInputDto: CreateChatRoomInputDto = req.body;
+  const { id: userId } = req.userInfo;
+
+  const createChatRoomDto: CreateChatRoomDto = {
+    title: createChatRoomInputDto.title,
+    userId
+  };
+  try {
+    const result = await companyChatRoomService.createChatRoom(
+      createChatRoomDto
+    );
+
+    if (result instanceof Error) {
+      throw result;
+    }
+    return res.status(statusCode.CREATED).json(result);
+  } catch (error: any) {
+    return res
+      .status(error.statusCode)
+      .json(fail(error.statusCode, error.message));
+  }
+};
+
 export default {
-  getChatroomsInfo
+  getChatRoomsInfo,
+  createChatRoom
 };
